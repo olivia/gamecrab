36496927c34fb1f09c100491f04c98cba8c18f70
Olivia Briggs <ofbriggs@gmail.com>
2017-01-04 19:26:55 -0500

    Complete op map

--- src/main.rs
+++ src/main.rs
@@ -18,38 +18,66 @@ enum Cond {
 #[derive(Debug)]
 enum OpCode {
     ADD(Register, Register),
+    ADD_d8(Register, u8),
+    ADD_r8(Register, i8),
+    ADD_C_d8(Register, u8),
     ADD_C(Register, Register),
     AND(Register),
+    AND_d8(u8),
     BIT(u8, Register),
     CALL(Register),
     CALL_C(Cond, Register),
     CP(Register),
+    CP_d8(u8),
+    CPL,
+    CCF,
     DEC(Register),
     DEC_F(Register),
     DI,
+    EI,
     ERR(String),
     HALT,
     INC(Register),
     INC_F(Register),
     JP(u16),
+    JP_HL,
     JP_C(Cond, u16),
+    JR(i8),
     JR_C(Cond, i8),
     LD(Register, B),
     LD_R(Register, Register),
     NOP,
     OR(Register),
+    OR_d8(u8),
     POP(Register),
     PUSH(Register),
     RET,
     RETI,
     RET_C(Cond),
     RES(u8, Register),
+    RLC(Register),
+    RLCA,
+    RRC(Register),
+    RRCA,
+    RL(Register),
+    RLA,
+    RR(Register),
+    RRA,
+    DAA,
+    SLA(Register),
+    SRA(Register),
+    SWAP(Register),
+    SRL(Register),
+    SCF,
     RST(u8),
     SET(u8, Register),
     STOP,
     SUB(Register),
+    SUB_d8(u8),
     SUB_C(Register, Register),
-    XOR(Register)
+    SUB_C_d8(Register, u8),
+    XOR(Register),
+    XOR_d8(u8)
 }
 
 //struct ROpCode<A> {
@@ -92,6 +120,7 @@ enum Register {
     HLM,
     ADDR(u16), 
     SP, // Stack Pointer
+    SP_OFF(i8), // stack pointer + offset
     PC // Program Counter
 }
 
@@ -103,10 +132,19 @@ fn get_arg(start:usize, num:u8, res:&Vec<u8>) -> u16 {
 }
 
 fn get_cb(start:usize, y:&Vec<u8>) -> (usize, OpCode, u8) {
+    let b = y[start + 1];
     match y[start + 1] {
-        b @ 0x40...0x7F => (2, OpCode::BIT((b - 0x40) / 8, lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
-        b @ 0x80...0xBF => (2, OpCode::RES((b - 0x80) / 8, lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
-        b @ 0xC0...0xFF => (2, OpCode::SET((b - 0xC0) / 8, lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x00...0x07 => (2, OpCode::RLC(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x08...0x0F => (2, OpCode::RRC(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x10...0x17 => (2, OpCode::RL(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x18...0x1F => (2, OpCode::RR(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x20...0x27 => (2, OpCode::SLA(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x28...0x2F => (2, OpCode::SRA(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x30...0x37 => (2, OpCode::SWAP(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x38...0x3F => (2, OpCode::SRL(lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x40...0x7F => (2, OpCode::BIT((b - 0x40) / 8, lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0x80...0xBF => (2, OpCode::RES((b - 0x80) / 8, lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
+        0xC0...0xFF => (2, OpCode::SET((b - 0xC0) / 8, lookup_mod_register(b)), 8 * lookup_mod_mult(b)),
         _ => (2, OpCode::ERR(format!("{:0>2X}", y[start])), 0)
     }
 }
@@ -162,6 +200,14 @@ fn lookup_op(start:usize, y:&Vec<u8>) -> (usize, OpCode, u8) {
         0x16 => (2, OpCode::LD(Register::D, B::One(y[start + 1])), 8),
         0x26 => (2, OpCode::LD(Register::H, B::One(y[start + 1])), 8),
         0x36 => (2, OpCode::LD(Register::HL, B::One(y[start + 1])), 12),
+        0x07 => (1, OpCode::RLCA, 4),
+        0x17 => (1, OpCode::RLA, 4),
+        0x27 => (1, OpCode::DAA, 4),
+        0x37 => (1, OpCode::SCF, 4),
+        0x08 => (3, OpCode::LD_R(Register::ADDR(get_arg(start, 3, y)), Register::SP), 20),
+        0x18 => (2, OpCode::JR(y[start + 1] as i8), 4),
+        0x28 => (2, OpCode::JR_C(Cond::Z, y[start + 1] as i8), 4), // 12/8
+        0x38 => (2, OpCode::JR_C(Cond::C, y[start + 1] as i8), 4),
         0x09 => (1, OpCode::ADD(Register::HL, Register::BC), 8),
         0x19 => (1, OpCode::ADD(Register::HL, Register::DE), 8),
         0x29 => (1, OpCode::ADD(Register::HL, Register::HL), 8),
@@ -182,6 +228,10 @@ fn lookup_op(start:usize, y:&Vec<u8>) -> (usize, OpCode, u8) {
         0x1E => (2, OpCode::LD(Register::E, B::One(y[start + 1])), 8),
         0x2E => (2, OpCode::LD(Register::L, B::One(y[start + 1])), 8),
         0x3E => (2, OpCode::LD(Register::A, B::One(y[start + 1])), 8),
+        0x0F => (1, OpCode::RRCA, 4),
+        0x1F => (1, OpCode::RRA, 4),
+        0x2F => (1, OpCode::CPL, 4),
+        0x3F => (1, OpCode::CCF, 4),
         0x0A => (1, OpCode::LD_R(Register::A, Register::BC), 8),
         0x1A => (1, OpCode::LD_R(Register::A, Register::DE), 8),
         0x2A => (1, OpCode::LD_R(Register::A, Register::HLP), 8),
@@ -197,6 +247,7 @@ fn lookup_op(start:usize, y:&Vec<u8>) -> (usize, OpCode, u8) {
         0xC4 => (3, OpCode::CALL_C(Cond::NZ, Register::ADDR(get_arg(start, 3, y))), 24), // 24/12
         0xD4 => (3, OpCode::CALL_C(Cond::NC, Register::ADDR(get_arg(start, 3, y))), 24), // 24/12
         0xCB => get_cb(start, y),
+        0xFB => (1, OpCode::EI, 4),
         0xCC => (3, OpCode::CALL_C(Cond::Z, Register::ADDR(get_arg(start, 3, y))), 24), // 24/12
         0xDC => (3, OpCode::CALL_C(Cond::C, Register::ADDR(get_arg(start, 3, y))), 24), // 24/12
         0xCD => (3, OpCode::CALL(Register::ADDR(get_arg(start, 3, y))), 24),
@@ -220,12 +271,30 @@ fn lookup_op(start:usize, y:&Vec<u8>) -> (usize, OpCode, u8) {
         0xD5 => (1, OpCode::PUSH(Register::DE), 16),
         0xE5 => (1, OpCode::PUSH(Register::HL), 16),
         0xF5 => (1, OpCode::PUSH(Register::AF), 16),
-        0xC7 => (1, OpCode::RST(0x00), 16),
+        0xC6 => (2, OpCode::ADD_d8(Register::A, y[start + 1]), 8),
+        0xD6 => (2, OpCode::SUB_d8(y[start + 1]), 8),
+        0xE6 => (2, OpCode::AND_d8(y[start + 1]), 8),
+        0xF6 => (2, OpCode::OR_d8(y[start + 1]), 8),
+        0xC7 => (1, OpCode::RST(0x00), 8),
         0xD7 => (1, OpCode::RST(0x10), 16),
         0xE7 => (1, OpCode::RST(0x20), 16),
         0xF7 => (1, OpCode::RST(0x30), 16),
         0xC8 => (1, OpCode::RET_C(Cond::Z), 8), // actually 20/8
         0xD8 => (1, OpCode::RET_C(Cond::C), 8), // actually 20/8
+        0xE8 => (2, OpCode::ADD_r8(Register::SP, y[start + 1] as i8), 16),
+        0xF8 => (2, OpCode::LD_R(Register::HL, Register::SP_OFF(y[start + 1] as i8)), 12),
+        0xC9 => (1, OpCode::RET, 16),
+        0xD9 => (1, OpCode::RETI, 16),
+        0xE9 => (1, OpCode::JP_HL, 4),
+        0xF9 => (1, OpCode::LD_R(Register::SP, Register::HL), 8),
+        0xCA => (3, OpCode::JP_C(Cond::Z, get_arg(start, 3, y)), 16), // 16/12
+        0xDA => (3, OpCode::JP_C(Cond::C, get_arg(start, 3, y)), 16),
+        0xEA => (3, OpCode::LD_R(Register::ADDR(get_arg(start, 3, y)), Register::A), 16),
+        0xFA => (3, OpCode::LD_R(Register::A, Register::ADDR(get_arg(start, 3, y))), 16),
+        0xCE => (2, OpCode::ADD_C_d8(Register::A, y[start + 1]), 8),
+        0xDE => (2, OpCode::SUB_C_d8(Register::A, y[start + 1]), 8),
+        0xEE => (2, OpCode::XOR_d8(y[start + 1]), 8),
+        0xFE => (2, OpCode::CP_d8(y[start + 1]), 8),
         0xCF => (1, OpCode::RST(0x08), 16),
         0xDF => (1, OpCode::RST(0x18), 16),
         0xEF => (1, OpCode::RST(0x28), 16),
